809adf43390f47c90957204dbe9aa9f761a0742b
István Donkó <istvan.donko+github@gmail.com>
2019-07-13 20:58:58 +0200

    Fixed display of numbers below one

--- www/scripts/shared/utilities.js
+++ www/scripts/shared/utilities.js
@@ -12,7 +12,8 @@ const normSquared = (x, y) => (x ** 2) + (y ** 2)
 const isPowerOfTwo = x => Math.log2(x) % 1 === 0
 
 const toNDigits = (n, x) => {
-  const offset = 10 ** (n - 1 - Math.floor(Math.log10(x)))
+  const correction = x < 1 ? 1 : 0
+  const offset = 10 ** (n - 1 - Math.floor(Math.log10(x)) - correction)
   return Math.round(x * offset) / offset || 0
 }
 
